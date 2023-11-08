4156d961ee016648a1b19f40a64cb1b7a79256b5
Glenn Brennand <glennbrennand@hotmail.com>
2023-11-08 09:42:20 +0100

    ingredient (#8)

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ require_once("lib/database.php");
 require_once("lib/artikel.php");
 require_once("lib/user.php");
 require_once("lib/keuken_type.php");
+require_once("lib/ingredient.php");
 
 /// INIT
 $db = new database();
 $art = new artikel($db->getConnection());
 $usr = new user($db->getConnection());
 $keu = new keuken_type($db->getConnection());
+$ing = new ingredient($db->getConnection());
 
 
 /// VERWERK 
@@ -17,6 +19,7 @@ $data = $art->selecteerArtikel(1);
 $data_user = $usr->selecteerUser(2);
 $data_keuk = $keu->selecteerKeuken_Type(4);
 $hoogsteId = $usr->maximumId();
+$data_ingr = $ing->selecteerIngredient(1);
 
 /// RETURN
 var_dump($data);
@@ -27,3 +30,5 @@ var_dump($data_user);
 echo "<BR> Het hoogst ingevoegde ID is: " . $hoogsteId;
 echo "<BR><BR> Hier volgt een array van het keukentype met id 4: ";
 var_dump($data_keuk);
+echo "<BR>";
+var_dump($data_ingr);
