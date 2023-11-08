2a805aebd04055f9c5c9490510f3b1e04ea00f6a
Glenn Brennand <glennbrennand@hotmail.com>
2023-11-08 16:44:20 +0100

    #10

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $art = new artikel($db->getConnection());
 $usr = new user($db->getConnection());
 $keu = new keuken_type($db->getConnection());
 $ing = new ingredient($db->getConnection());
-
+$gei = new gerecht_info($db->getConnection());
 
 
 
@@ -24,13 +24,24 @@ $data_user = $usr->selecteerUser(2);
 $data_keuk = $keu->selecteerKeuken_Type(4);
 $data_ingr = $ing->selecteerIngredient(1);
 
-
+$data_info_opmerking = $gei->selectRecordType('O');
+$data_info_favoriet = $gei->selectRecordType('F');
+$data_geri = $gei->selecteerGerecht_info(1);
 
 
 /// RETURN
 var_dump($data_ingr);
 echo "<br><br><br>";
+var_dump($data_geri);
 echo "<br><br><br>";
+var_dump($data_info_opmerking);
 echo "<br><br><br>";
 
+foreach ($data_ingr as $ingredient) { ?>
+    <tr>
+        <td><?php echo "id: " . $ingredient['ingredient_id']; ?></td><br>
+        <td><?php echo "Ingredient: " . $ingredient['ingredient_naam']; ?></td><br>
+        <td><?php echo "aantal: " . $ingredient['aantal']; ?></td><br>
+    </tr>
+<?php } ?>
 
--- lib/gerecht_info.php
+++ lib/gerecht_info.php
@@ -12,44 +12,50 @@ class gerecht_info {
 
         $sql = "SELECT * 
                 FROM gerecht_info 
-                WHERE gerecht.id = $gerecht_id";
+                WHERE gerecht_id = $gerecht_id";
 
         $result = mysqli_query($this->connection, $sql);
         if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()){
-                echo "alle informatie: " . $row["id, record_type, user_id, datum, numeriekveld, tekstveld"] . "<br>";
+            while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                $return[] = $row;
             }
-        }
-        else {
+        } else {
             echo "0 results";
-          }
+        }
+    
+        return $return;
     }
 
     public function selectRecordType($recordtype){
 
         $sql = "SELECT * 
-        FROM gerecht_info 
-        WHERE record_type = '$recordtype'";
+                FROM gerecht_info 
+                WHERE record_type = '$recordtype'";
         $result = mysqli_query($this->connection, $sql);
         if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()){
-                echo "record_type: " . $row["record_type"] . ", nummeriekveld: " . $row["nummeriekveld"] . ", tekstveld: " .$row["tekstveld"] . ", user: " . $row["user_id"] . "<br>";
+            while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                $return[] = $row;
             }
-        }
-        else {
+        } else {
             echo "0 results";
-          }
+        }
+    
+        return $return;
     }
 
     public function addFavorite($gerecht_id, $user_id){
-        $sql = "INSERT INTO gerecht_info (gerecht_id, user_id, record_type) VALUES ($gerecht_id, $user_id, 'F')";
+        $sql = "INSERT INTO gerecht_info (gerecht_id, user_id, record_type) 
+                VALUES ($gerecht_id, $user_id, 'F')";
         $result = mysqli_query($this->connection, $sql);
 
         return($result);
     }
 
     public function deleteFavorite($gerecht_id, $user_id){
-        $sql = "DELETE FROM gerecht_info WHERE gerecht_id = $gerecht_id AND user_id = $user_id AND record_type = 'F'";
+        $sql = "DELETE FROM gerecht_info 
+                WHERE gerecht_id = $gerecht_id 
+                AND user_id = $user_id 
+                AND record_type = 'F'";
         $result = mysqli_query($this->connection, $sql);
 
         return($result);
